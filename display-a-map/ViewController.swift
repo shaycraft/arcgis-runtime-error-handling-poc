31b91f6294d04fc6d2495fa3f58f864f36a3ea1d
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-07 22:21:38 -0700

    refactor: changes verbiage of alert buttons

--- display-a-map/ViewController.swift
+++ display-a-map/ViewController.swift
@@ -1,18 +1,3 @@
-// Copyright 2020 Esri
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 import UIKit
 
 import ArcGIS
@@ -20,7 +5,6 @@ import ArcGIS
 class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: AGSMapView!
-    
     
     private func   setupMap() throws -> AGSFeatureLayer {
         
@@ -34,6 +18,7 @@ class ViewController: UIViewController {
             let trailheadsTable = AGSServiceFeatureTable(url: featureServiceURL)
             return AGSFeatureLayer(featureTable: trailheadsTable)
         }()
+        
         map.operationalLayers.add(featureLayer)
         
         featureLayer.load { [weak self] (error: Error?) in
@@ -87,11 +72,11 @@ class ViewController: UIViewController {
     
     @IBAction func displayErrorAlert(error: String) {
         // Create the action buttons for the alert.
-        let defaultAction = UIAlertAction(title: "Yikes!",
+        let defaultAction = UIAlertAction(title: "Lorem Ipsum",
                                           style: .default) { (action) in
             // Respond to user selection of the action.
         }
-        let cancelAction = UIAlertAction(title: "Whatevs",
+        let cancelAction = UIAlertAction(title: "Cancel",
                                          style: .cancel) { (action) in
             // Respond to user selection of the action.
         }
