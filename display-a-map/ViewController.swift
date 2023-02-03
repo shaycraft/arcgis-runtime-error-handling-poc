a29490feffa50b68d3cd50ec4981ffa11de68834
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-02-03 00:11:15 -0700

    multiple layers start

--- display-a-map/ViewController.swift
+++ display-a-map/ViewController.swift
@@ -7,7 +7,7 @@ extension ViewController: AGSAuthenticationManagerDelegate {
         // NOTE: Never hardcode login information in a production application. This is done solely for the sake of the sample.
         printDebugMessage(message: "We been challenged!")
         // need to get token from token endpoint and paste here
-        let credentials = AGSCredential(token: "beBS5ASXeNTRsjBG3-b9oLr0rE7qx2JsCTrV56gGIGs.", referer: "http://localhost")
+        let credentials = AGSCredential(token: "YKXamjz1XW43-SsAlEh-cjiAvMiHrHf_6RCR8sFWfrs.", referer: "http://localhost")
         challenge.continue(with: credentials)
     }
 }
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: AGSMapView!
     
-    private func   setupMap() throws -> AGSFeatureLayer {
+    private func setupMap() throws -> AGSFeatureLayer {
         
         let map = AGSMap(
             basemapStyle: .arcGISTopographic
@@ -26,11 +26,12 @@ class ViewController: UIViewController {
         
         let featureLayer: AGSFeatureLayer = {
             // Malform this URL to test errors
-//            let url = "https://foh1kgodaj.execute-api.us-west-2.amazonaws.com/arcgis/foo"
-//            let url = "https://xho1ow9dj5.execute-api.us-west-2.amazonaws.com/arcgis/foo"
+            //            let url = "https://foh1kgodaj.execute-api.us-west-2.amazonaws.com/arcgis/foo"
+            //            let url = "https://xho1ow9dj5.execute-api.us-west-2.amazonaws.com/arcgis/foo"
+            //             let url = "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+            
             // dev region url endpoint
-            let url = "https://fjk1l1iw9b.execute-api.us-east-2.amazonaws.com/arcgis/rest/services/GFEE/Gas_Transmission/FeatureServer/28"
-//             let url = "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+            let url = "https://fjk1l1iw9b.execute-api.us-east-2.amazonaws.com/arcgis/rest/services/GFEE/Land/FeatureServer/6"
             let featureServiceURL = URL(string: url)!
             let trailheadsTable = AGSServiceFeatureTable(url: featureServiceURL)
             return AGSFeatureLayer(featureTable: trailheadsTable)
@@ -47,16 +48,39 @@ class ViewController: UIViewController {
                 // no error, can do dependent processing here
                 
                 map.operationalLayers.add(featureLayer)
-
+                
             }
         }
+        
+        let featureLayerAnno: AGSFeatureLayer = {
+            let lotAnnoUrl = URL(string: "https://fjk1l1iw9b.execute-api.us-east-2.amazonaws.com/arcgis/rest/services/GFEE/Land/FeatureServer/1")!
+            let annoTable = AGSServiceFeatureTable(url: lotAnnoUrl)
+            return AGSFeatureLayer(featureTable: annoTable)
+        }()
+        
+//        featureLayerAnno.load { [weak self] (error: Error?) in
+//            guard let self = self else { return }
+//            if let error = error {
+//                self.printDebugMessage(message:  "[ANNO LOAD CALLBACK], ERROR: \(error)")
+//                self.displayErrorAlert(error: "Error loading map image layer: \(error.localizedDescription)")
+//            } else {
+//                self.printDebugMessage(message: "[ANNO LOAD CALLBACK]: SUCCESS, no error")
+//                // no error, can do dependent processing here
+//
+//                map.operationalLayers.add(featureLayerAnno)
+//
+//            }
+//        }
+        map.operationalLayers.add(featureLayerAnno)
+        
+        
         mapView.map = map
         
         mapView.setViewpoint(
             AGSViewpoint(
-                latitude: 34.02700,
-                longitude: -118.71511,
-                scale: 200_000
+                latitude: 39.72700,
+                longitude: -104.85511,
+                scale: 30_000
             )
         )
         
@@ -79,7 +103,7 @@ class ViewController: UIViewController {
         do {
             let mapResult = try setupMap()
             self.printDebugMessage(message: "[BEFORE TIME DELAY], loadStatus:  " + String(mapResult.loadStatus.rawValue))
-           
+            
             DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
                 self.printDebugMessage(message: "[AFTER TIME DELAY], loadError:   " + (mapResult.loadError?.localizedDescription ?? "[EMPTY]"))
                 self.printDebugMessage(message: "[AFTER TIME DELAY], loadStatus: " + String(mapResult.loadStatus.rawValue))
